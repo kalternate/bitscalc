7ba2e5d868a02dded526f64ccb128c8bd11724be
Keagan <keaganmedwards@gmail.com>
2024-05-18 16:17:37 -0700

    Add src code link

--- site/src/lib/FormatButton.svelte
+++ site/src/lib/FormatButton.svelte
@@ -9,11 +9,11 @@
 </script>
 
 {#if format === myFormat}
-<button on:click={onClick} class="font-mono hover:text-sky-300 underline text-sm">
-    <slot/>
-</button>
+    <button on:click={onClick} class="font-mono underline text-sm">
+        <slot/>
+    </button>
 {:else}
-<button on:click={onClick} class="font-mono hover:text-sky-300 underline text-sm decoration-zinc-600">
-    <slot/>
-</button>
+    <button on:click={onClick} class="font-mono hover:text-sky-300 underline text-sm text-zinc-400">
+        <slot/>
+    </button>
 {/if}
--- site/src/lib/Panel.svelte
+++ site/src/lib/Panel.svelte
@@ -17,5 +17,11 @@
         <div class="font-mono">
             Bitscalc is a binary integer calculator for quickly evaluating programming expressions. It supports arithmetic, logical, and bitwise operators with C-like precedence. Step-by-step results are shown in decimal, hexadecimal, and binary. Enter an expression above to get started.
         </div>
+        <div class="font-mono mt-2 text-zinc-400">
+            v1.0 —
+            <a href="https://github.com/kalternate/bitscalc" class="underline hover:text-sky-300">
+                source
+            </a>
+        </div>
     {/if}
 </div>
