d28af10d647c91cb4ba169ed18edc2251274b8b5
kalternate <keaganmedwards@gmail.com>
2024-05-20 12:40:51 -0700

    Better binary number formatting

--- lib/src/value.rs
+++ lib/src/value.rs
@@ -12,7 +12,29 @@ impl FormattedValue {
         FormattedValue {
             dec: format!("{}", value),
             hex: format!("{:#x}", value),
-            bin: format!("{:#b}", value),
+            bin: make_bin_format(value),
         }
     }
+}
+
+fn make_bin_format(value: i64) -> String {
+    let unspaced: Vec<char> = format!("{:b}", value).chars().collect();
+    let mut spaced = String::new();
+
+    if unspaced.len() % 4 != 0 {
+        for _ in 0..4-(unspaced.len() % 4) {
+            spaced.push('0')
+        }
+    }
+
+    for i in 0..unspaced.len() {
+        let l = unspaced.len()-i;
+        if l % 4 == 0 {
+            spaced.push(' ')
+        }
+
+        spaced.push(unspaced[i]);
+    }
+
+    spaced
 }
